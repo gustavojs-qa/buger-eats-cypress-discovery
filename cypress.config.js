882038b5d871823ad1afa481c646d23c7f17cfa4
Gustavo Silva <gustavo.js@live.com>
2022-12-15 00:23:50 -0300

    meu primeiro commit no git

---
+++ cypress.config.js
@@ -0,0 +1,23 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },    
+    viewportWidth: 1440,
+    viewportHeight: 900,
+    baseUrl: 'https://buger-eats-qa.vercel.app',
+    testIsolation: false
+  },
+});
+
+// const { defineConfig } = require("cypress");
+
+// module.exports = defineConfig({
+//   e2e: {
+//     setupNodeEvents(on, config) {
+//       // implement node event listeners here
+//     },
+//   },
+// });
